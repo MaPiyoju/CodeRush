03c3965ccbc792debf08fd580a1bd59c384e1b26
Camila Molina <camimolina0514@gmail.com>
2023-11-28 18:41:49 -0500

    Add max time and streak -cast firstLogin

--- Assets/Scripts/Profile/ProfileScript.cs
+++ Assets/Scripts/Profile/ProfileScript.cs
@@ -1,11 +1,17 @@
 using Firebase.Firestore;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class ProfileScript : MonoBehaviour
 {
+    private LoadStatsScript loadStatsScript;
+
     public GameObject nameText;
     private TextMeshProUGUI nameValue;
     public GameObject aliasText;
@@ -24,31 +30,99 @@ public class ProfileScript : MonoBehaviour
 
     private void Awake()
     {
-        nameValue = nameText.GetComponent<TextMeshProUGUI>();
-        nameValue.text = Globals.usuario.name;
+       if(Globals.usuario != null)
+        {
+            loadStatsScript = GetComponent<LoadStatsScript>();
 
-        Debug.Log("nombre: "+nameValue.text);
+            nameValue = nameText.GetComponent<TextMeshProUGUI>();
+            nameValue.text = Globals.usuario.name;
 
-        aliasValue = aliasText.GetComponent<TextMeshProUGUI>();
-        aliasValue.text = Globals.usuario.alias;
+            aliasValue = aliasText.GetComponent<TextMeshProUGUI>();
+            aliasValue.text = Globals.usuario.alias;
 
-        dateValue = dateText.GetComponent<TextMeshProUGUI>();
-        System.DateTime first_login = Globals.usuario.first_login;
-        dateValue.text = "Usuario desde "+first_login.ToString();
+            dateValue = dateText.GetComponent<TextMeshProUGUI>();
+            System.DateTime first_login = Globals.usuario.first_login;
+            CultureInfo ci = new CultureInfo("es-ES");
+            string FechaFormateada = first_login.ToString("MMMM", ci) + " del " + first_login.ToString("yyyy");
+            FechaFormateada = char.ToUpper(FechaFormateada[0]) + FechaFormateada.Substring(1);
+            dateValue.text = "Usuario desde " + FechaFormateada;
 
-        highestStreakValue = highestStreakText.GetComponent<TextMeshProUGUI>();
-        highestStreakValue.text = Globals.usuario.streak.ToString();
+            highestStreakValue = highestStreakText.GetComponent<TextMeshProUGUI>();
+            highestStreakValue.text = Globals.usuario.streak.ToString();
 
-        categoryValue = categoryText.GetComponent<TextMeshProUGUI>();
-        categoryValue.text = Globals.usuario.exp.ToString();
+            categoryValue = categoryText.GetComponent<TextMeshProUGUI>();
+            categoryValue.text = DeterminarNivelExperiencia(Globals.usuario.exp);
 
-        longerTimeValue = longerTimeText.GetComponent<TextMeshProUGUI>();
-        longerTimeValue.text = "222";
+            int maxTime = Globals.usuario.history.Max(x => x.time);
+            longerTimeValue = longerTimeText.GetComponent<TextMeshProUGUI>();
+            longerTimeValue.text = ConvertirSegundosAMinutos(maxTime);
 
-        streakDaysValue = streakDaysText.GetComponent<TextMeshProUGUI>();
-        streakDaysValue.text = "5";
+            if(Globals.usuario.history.Count() > 0)
+            {
+                IEnumerable<DateTime> days = Globals.usuario.history.Select(x => x.date);
+                int streakDays = GetConsecutiveDays(Globals.usuario.history.ToArray()[0].date, days).Count();
+                streakDaysValue = streakDaysText.GetComponent<TextMeshProUGUI>();
+                streakDaysValue.text = streakDays.ToString();
+            }
+        }
+    }
 
-        // max tiempo max scor recorrer la lista de array y filtar el más alto
+    public string ConvertirSegundosAMinutos(int segundos)
+    {
+        int hor, min, seg;
+        hor = (segundos / 3600);
+        min = ((segundos - hor * 3600) / 60);
+        seg = segundos - (hor * 3600 + min * 60);
+        return min + ":" + seg;
+    }
+
+    static IEnumerable<DateTime> GetConsecutiveDays(DateTime startDate, IEnumerable<DateTime> days)
+    {
+        var wantedDate = startDate;
+        foreach (var day in days.Where(d => d >= startDate).OrderBy(d => d))
+        {
+            if (day == wantedDate)
+            {
+                yield return day;
+                wantedDate = wantedDate.AddDays(1);
+            }
+            else
+            {
+                yield break;
+            }
+        }
+    }
+
+    public string DeterminarNivelExperiencia(int exp)
+    {
+        if (exp >= 0 && exp < 2000)
+        {
+            return "Newbie";
+        }
+        else if (exp >= 2000 && exp < 4000)
+        {
+            return "Gateador";
+        }
+        else if (exp >= 4000 && exp < 6000)
+        {
+            return "Iniciado";
+        }
+        else if (exp >= 6000 && exp < 8000)
+        {
+            return "Aprendiz";
+        }
+        else if (exp >= 8000 && exp < 10000)
+        {
+            return "Experto";
+        }
+        else if (exp >= 10000)
+        {
+            return "Pro";
+        }
+        else
+        {
+            return "Desconocido"; // Valor por defecto si el rango no se encuentra
+        }
     }
 
     // Start is called before the first frame update
--- Assets/Scripts/stats/statsLoad.cs
+++ Assets/Scripts/stats/statsLoad.cs
@@ -60,7 +60,7 @@ public class LoadStatsScript : MonoBehaviour
         MostrarUsuarios(usuariosFiltrados.ToArray());
     }
 
-    private string DeterminarNivelExperiencia(int exp)
+    public string DeterminarNivelExperiencia(int exp)
     {
         if (exp >= 0 && exp < 2000)
         {
@@ -124,10 +124,11 @@ public class LoadStatsScript : MonoBehaviour
         }
     }
 
-    private void obtenerExperienciaUsuario()
+    public string obtenerExperienciaUsuario()
     {
         experiencia = DeterminarNivelExperiencia(Globals.exp);
         //Debug.Log("EXPERIENCIA="+experiencia);
+        return experiencia;
     }
 
     // Update is called once per frame
